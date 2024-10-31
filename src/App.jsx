76c42b3689e85bd5db9550c0dc56cc9b5a556ad7
Mustapha Said <mustaphasaid@Mustaphas-MacBook-Air.local>
2024-10-31 12:07:47 +0100

    design fix

--- src/App.jsx
+++ src/App.jsx
@@ -7,19 +7,9 @@ function App() {
   return (
     <div className="min-h-screen bg-gray-100">
       <Navbar />
-      <div className="p-4 flex justify-between items-center">
-        <h1 className="text-2xl font-bold">Bienvenue sur JobBoard</h1>
-      </div>
-      <div className="p-4">
-        <div className="bg-white p-6 rounded-lg shadow-lg">
-          <div className="flex gap-6 justify-center">
-            <img
-              src="https://upload.wikimedia.org/wikipedia/commons/a/a9/Amazon_logo.svg"
-              alt="Amazon Logo"
-              width="100"
-            />
-          </div>
-        </div>
+      <div className="p-6 flex flex-col justify-center text-center items-center bg-gradient-to-r from-blue-500 to-purple-600 text-white">
+        <h1 className="text-3xl font-extrabold mb-4">Bienvenue sur JobBoard</h1>
+        <div className="p-4 w-full max-w-md"></div>
       </div>
       <JobBoard />
     </div>
--- src/components/NotificationsButton.jsx
+++ src/components/NotificationsButton.jsx
@@ -1,16 +1,15 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 function NotificationsButton() {
   const [notifications, setNotifications] = useState([]);
 
   useEffect(() => {
-
-    requestPermissionToShowNotifications()
+    requestPermissionToShowNotifications();
 
     // Écouter les notifications reçues depuis le service worker
     if (navigator.serviceWorker) {
       navigator.serviceWorker.addEventListener("message", (event) => {
-        console.log(event)
+        console.log(event);
         const newNotification = event.data;
         setNotifications((prev) => [newNotification, ...prev]);
       });
@@ -18,78 +17,72 @@ function NotificationsButton() {
   }, []);
 
   function urlBase64ToUint8Array(base64String) {
-    const padding = '='.repeat((4 - base64String.length % 4) % 4);
-    const base64 = (base64String + padding).replace(/-/g, '+').replace(/_/g, '/');
+    const padding = "=".repeat((4 - (base64String.length % 4)) % 4);
+    const base64 = (base64String + padding)
+      .replace(/-/g, "+")
+      .replace(/_/g, "/");
     const rawData = window.atob(base64);
-    return new Uint8Array([...rawData].map(char => char.charCodeAt(0)));
+    return new Uint8Array([...rawData].map((char) => char.charCodeAt(0)));
   }
 
-  function subscribeToPushNotifications()
-  {
-    navigator.serviceWorker.getRegistration().then(registration => {
-        
-        fetch("http://localhost:5000/vapidPublicKey")
-        .then(response => response.json())
-        .then(data => {
+  function subscribeToPushNotifications() {
+    navigator.serviceWorker.getRegistration().then((registration) => {
+      fetch("http://localhost:5500/vapidPublicKey")
+        .then((response) => response.json())
+        .then((data) => {
           const publicVapidKey = data.publicVapidKey;
 
-          registration.pushManager.subscribe({
-            userVisibleOnly: true,
-            applicationServerKey: urlBase64ToUint8Array(publicVapidKey)
-          })
-          .then(newSubscription => {
-            fetch("http://localhost:5000/subscribe", {
-              method: 'POST',
-              headers: { 'Content-Type': 'application/json' },
-              body: JSON.stringify(newSubscription)
+          registration.pushManager
+            .subscribe({
+              userVisibleOnly: true,
+              applicationServerKey: urlBase64ToUint8Array(publicVapidKey),
+            })
+            .then((newSubscription) => {
+              fetch("http://localhost:5500/subscribe", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(newSubscription),
+              });
             });
-          })
-        })
-      })
+        });
+    });
   }
 
-  function requestPermissionToShowNotifications()
-  {
-    if(window.Notification) 
-      {
-        if(Notification.permission === "granted")
-        {
-            console.log("Permission to show notifications had already been granted.");
-            subscribeToPushNotifications()
-        }
-        else if(Notification.permission === "denied")
-        {
-            console.log("Permission to show notifications was denied.");
-        }
-        else {
-          Notification.requestPermission().then(permission => {
-  
-            if(permission === "granted")
-            {
-              console.log("Notification permission granted.")
-              subscribeToPushNotifications()
-            }
-            else if(permission === "denied")
-            {
-              console.log("Notification permission denied.")
-            }
-            else {
+  function requestPermissionToShowNotifications() {
+    if (window.Notification) {
+      if (Notification.permission === "granted") {
+        console.log(
+          "Permission to show notifications had already been granted."
+        );
+        subscribeToPushNotifications();
+      } else if (Notification.permission === "denied") {
+        console.log("Permission to show notifications was denied.");
+      } else {
+        Notification.requestPermission()
+          .then((permission) => {
+            if (permission === "granted") {
+              console.log("Notification permission granted.");
+              subscribeToPushNotifications();
+            } else if (permission === "denied") {
+              console.log("Notification permission denied.");
+            } else {
               console.log("Notification permission dismissed");
             }
           })
-          .catch(error => {
-            console.log('Error requesting notification permission:', error)
-          })
-        }
-  
+          .catch((error) => {
+            console.log("Error requesting notification permission:", error);
+          });
       }
+    }
   }
 
   return (
     <div>
       <button
         className="bg-blue-500 text-white px-4 py-2 rounded"
-        onClick={() => alert(notifications.map((notif) => notif.title).join("\n"))}
+        onClick={() =>
+          alert(notifications.map((notif) => notif.title).join("\n"))
+        }
       >
         Notifications ({notifications.length})
       </button>
